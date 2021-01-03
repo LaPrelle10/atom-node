d8ca35d31721ccd79b7d456deb674de85d2fca39
LaPrelle10 <71919081+LaPrelle10@users.noreply.github.com>
2021-01-03 14:40:05 -0800

    exe
    setup.exe for rendering processor unit setup

---
+++ buildpacks/node.js
@@ -0,0 +1,20 @@
+# NODE.JS 
+
+# Source 
+https://nodejs.org/en/docs/guides/getting-started-guide/
+
+Build pack for the creation of the atom-node node.js is a computer language built for creating node's to connect to for serveral different purposes 
+such being an image result for node.js purpose node.js is primarily used for non-blocking, event-driven servers, due to its single-threaded nature. 
+It's used for traditional web sites and back-end API services, but was designed with real-time, push-based architectures in mind.
+
+Use of the API Connection is est. for connection purpose and keeping your stuff private while creating a node for cryptocurrency also using Mircosoft 
+or Windows systems helps with the creation of the blockchain node or atom-node.
+
+# Source 
+https://learn.vonage.com/blog/2020/04/24/connect-to-the-ethereum-blockchain-with-node-js-and-vonage-dr
+
+using this site to help as well for node.js building a atom-node. Make an account and press forth to node.js node building purposes for blockchain 
+and cryptocurrency linking and sending and receiving payments keeping a log of your transactions in your atom-ledger. 
+
+Crypto ledger cash advancement is generated throughly with money storing features policy procedure rendered unit setup net spending process protocol featured 
+lead setup request. User requires system to set up proper allowing correct features to follow through.
